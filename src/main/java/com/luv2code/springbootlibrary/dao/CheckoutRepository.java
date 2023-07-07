a84ea257cd08c5f004e4a92c89067878204e9600
Pierre Lasante <pierre.lasante@videotron.ca>
2023-07-07 09:06:09 -0400

    Implementing BookService checkout

---
+++ src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -0,0 +1,8 @@
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
